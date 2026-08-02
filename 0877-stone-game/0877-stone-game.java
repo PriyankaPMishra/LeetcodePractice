4a55e7c5f5b682216cc5a18a884a887fb809e1ff
Priyanka Mishra <111686692+PriyankaPMishra@users.noreply.github.com>
2026-08-02 18:05:20 +0530

    Time: 8 ms (18.78%), Space: 46.8 MB (13.54%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,26 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        // dp[i][j] = maximum stones the current player can get from piles[i..j]
+        int[][] dp = new int[n][n];
+        
+        // Base case: one pile
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        
+        // Fill for longer intervals
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i <= n - length; i++) {
+                int j = i + length - 1;
+                // Current player takes left or right
+                dp[i][j] = Math.max(
+                    piles[i] - dp[i + 1][j],
+                    piles[j] - dp[i][j - 1]
+                );
+            }
+        }
+        
+        return dp[0][n - 1] > 0;
     }
 }
